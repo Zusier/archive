8d815460c4436fd6e3149f9589937dd0bb36f9c3
Zusier <46111482+Zusier@users.noreply.github.com>
2021-05-20 08:45:50 -0500

    Update Force Disable DEP.bat

--- Scripts/DEP/Force Disable DEP.bat
+++ Scripts/DEP/Force Disable DEP.bat
@@ -1,7 +1,6 @@
 @echo off
 
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "NoDataExecutionPrevention" /t REG_DWORD /d "1" /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "DisableHHDEP" /t REG_DWORD /d "1" /f
 bcdedit /set nx AlwaysOff
 powershell set-ProcessMitigation -System -Disablw DEP
 powershell set-ProcessMitigation -System -Disable EmulateAtlThunks
