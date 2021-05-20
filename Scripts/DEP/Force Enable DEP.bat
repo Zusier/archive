7ad26d27085c5bf8b7260d16511ef4b90e671e9d
Zusier <46111482+Zusier@users.noreply.github.com>
2021-05-20 08:46:09 -0500

    Update Force Enable DEP.bat

--- Scripts/DEP/Force Enable DEP.bat
+++ Scripts/DEP/Force Enable DEP.bat
@@ -1,7 +1,6 @@
 @echo off
 
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "NoDataExecutionPrevention" /t REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "DisableHHDEP" /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "NoDataExecutionPrevention" /t REG_DWORD /d "0" /f
 bcdedit /set nx AlwaysOn
 powershell set-ProcessMitigation -System -Enable DEP
 powershell set-ProcessMitigation -System -Enable EmulateAtlThunks
