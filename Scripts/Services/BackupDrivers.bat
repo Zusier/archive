988889dae88d3ec63568aeb1720b8e90359ec5e2
Zusier <zusier@disroot.org>
2021-09-12 12:12:45 -0700

    Improve Reliability + Driver Backup

--- Scripts/Services/BackupDrivers.bat
+++ Scripts/Services/BackupDrivers.bat
@@ -1,6 +1,6 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
-set filename=ServiceAndDriverBackup.reg
+set filename=DriverBackup.reg
 del /F %filename% >nul 2>&1
 
 echo Windows Registry Editor Version 5.00 >> %filename%
@@ -8,31 +8,31 @@ echo. >> %filename%
 echo ;Services Backup on %date% at%time% >> %filename%
 echo. >> %filename%
 
-for /f "tokens=1" %%i in ('reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services" /t REG_DWORD /s /f "Start"^| findstr "CurrentControlSet\Services"') do (
-	set svc=%%i
-	for /f "tokens=3" %%i in ('reg query "!svc!" /t REG_DWORD /s /c /f "Start" /e^| findstr "[0-4]$"') do (
-		set start=%%i
+for /f "delims=," %%i in ('driverquery /FO CSV') do (
+	set svc=%%~i
+	for /f "tokens=3" %%f in ('reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\%%~i" /t REG_DWORD /s /c /f "Start" /e^| findstr "[0-4]$"') do (
+		set start=%%f
 		if !start!==0x4 (
-			echo [!svc!] >> %filename%
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
 			echo "Start"=dword:00000004 >> %filename%
 			echo. >> %filename% )
 		if !start!==0x3 (
-			echo [!svc!] >> %filename%
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
 			echo "Start"=dword:00000003 >> %filename%
 			echo. >> %filename% )
 		if !start!==0x2 (
-			echo [!svc!] >> %filename%
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
 			echo "Start"=dword:00000002 >> %filename%
 			echo. >> %filename% )
 		if !start!==0x1 (
-			echo [!svc!] >> %filename%
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
 			echo "Start"=dword:00000001 >> %filename%
 			echo. >> %filename% )
 		if !start!==0x0 (
-			echo [!svc!] >> %filename%
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
 			echo "Start"=dword:00000000 >> %filename%
 			echo. >> %filename% )
-	)
+	) 
 ) >nul 2>&1
 
 notepad %filename%
--- Scripts/Services/BackupServices.bat
+++ Scripts/Services/BackupServices.bat
@@ -1,33 +1,38 @@
-:: 1. Get list of services.
-:: 2. Write reg path of services to reg file
-:: 3. Get state of services
-:: 4. Translate text state (e.g. Disabled) to number (e.g. 4)
-
 @echo off
 SETLOCAL EnableDelayedExpansion
+set filename=ServiceBackup.reg
+del /F %filename% >nul 2>&1
 
-del /F ServiceBackup.reg >nul 2>&1
-
-echo Windows Registry Editor Version 5.00 >> ServiceBackup.reg
-echo. >> ServiceBackup.reg
-echo ;Services and Drivers Backup on %date% at%time% >> ServiceBackup.reg
-echo. >> ServiceBackup.reg
-
+echo Windows Registry Editor Version 5.00 >> %filename%
+echo. >> %filename%
+echo ;Services Backup on %date% at%time% >> %filename%
+echo. >> %filename%
 for /f "skip=1" %%i in ('wmic service get Name^| findstr "[a-z]"^| findstr /V "TermService"') do (
 	set svc=%%i
 	set svc=!svc: =!
-	echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> ServiceBackup.reg
 	for /f "tokens=3" %%i in ('reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!" /t REG_DWORD /s /c /f "Start" /e^| findstr "[0-4]$"') do (
 		set start=%%i
-		if !start!==0x4 set start="Start"=dword:00000004
-		if !start!==0x3 set start="Start"=dword:00000003
-		if !start!==0x2 set start="Start"=dword:00000002
-		if !start!==0x1 set start="Start"=dword:00000001
-		if !start!==0x0 set start="Start"=dword:00000000
-		echo.!start! >> ServiceBackup.reg
-		echo. >> ServiceBackup.reg
+		if !start!==0x4 (
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
+			echo "Start"=dword:00000004 >> %filename%
+			echo. >> %filename% )
+		if !start!==0x3 (
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
+			echo "Start"=dword:00000003 >> %filename%
+			echo. >> %filename% )
+		if !start!==0x2 (
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
+			echo "Start"=dword:00000002 >> %filename%
+			echo. >> %filename% )
+		if !start!==0x1 (
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
+			echo "Start"=dword:00000001 >> %filename%
+			echo. >> %filename% )
+		if !start!==0x0 (
+			echo [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\!svc!] >> %filename%
+			echo "Start"=dword:00000000 >> %filename%
+			echo. >> %filename% )
 	)
+) >nul 2>&1
 
-)
-
-notepad ServiceBackup.reg
+notepad %filename%
